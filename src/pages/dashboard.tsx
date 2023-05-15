b849967d0ef6be6ab1785a60b183714763598b0e
Rafael Araujo <rafa123839@gmail.com>
2023-05-15 18:29:28 -0300

    Listagem responsiva

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -71,7 +71,7 @@ export default function Dashboard() {
                     alignContent="flex-start"
                 >
                     <Box
-                      p="8"
+                      p={["6", "8"]}
                       bg="gray.800"
                       borderRadius={8}
                       pb="4"
@@ -81,7 +81,7 @@ export default function Dashboard() {
                     </Box>
 
                     <Box
-                      p="8"
+                      p={["6", "8"]}
                       bg="gray.800"
                       borderRadius={8}
                       pb="4"
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+
     return (
         <Box>
             <Header />
@@ -29,17 +34,17 @@ export default function UserList() {
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4", "4","6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink"></Checkbox>
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                {isWideVersion && <Th>Data de cadastro</Th>}
                                 <Th width="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4","6"]}>
                                     <Checkbox colorScheme="pink"></Checkbox>
                                 </Td>
                                 <Td>
@@ -48,9 +53,7 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">rafa12@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>
-                                    <Text>04 de abril, 2023</Text>
-                                </Td>
+                                {isWideVersion && <Td>04 de abril, 2023</Td>}
                                 <Td>
                                     <Button
                                         as="a"
@@ -64,7 +67,7 @@ export default function UserList() {
                                 </Td>
                             </Tr>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4","6"]}>
                                     <Checkbox colorScheme="pink"></Checkbox>
                                 </Td>
                                 <Td>
@@ -73,9 +76,7 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">rafa12@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>
-                                    <Text>04 de abril, 2023</Text>
-                                </Td>
+                                {isWideVersion && <Td>04 de abril, 2023</Td>}
                                 <Td>
                                     <Button
                                         as="a"
@@ -91,7 +92,7 @@ export default function UserList() {
                         </Tbody>
                     </Table>
 
-                    <Pagination/>
+                    <Pagination />
                 </Box>
             </Flex>
         </Box>
