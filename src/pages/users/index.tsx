68b10086a3fb6cf3517d0cacf052532ae4687ee5
Rafael Araujo <rafa123839@gmail.com>
2023-05-27 08:08:49 -0300

    Listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,20 @@ export default function UserList() {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
-        return data
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        return users
     })
 
     const isWideVersion = useBreakpointValue({
@@ -66,54 +79,35 @@ export default function UserList() {
                                         </Tr>
                                     </Thead>
                                     <Tbody>
-                                        <Tr>
-                                            <Td px={["4", "4", "6"]}>
-                                                <Checkbox colorScheme="pink"></Checkbox>
-                                            </Td>
-                                            <Td>
-                                                <Box>
-                                                    <Text fontWeight="bold">Rafael araujo</Text>
-                                                    <Text fontSize="sm" color="gray.300">rafa12@gmail.com</Text>
-                                                </Box>
-                                            </Td>
-                                            {isWideVersion && <Td>04 de abril, 2023</Td>}
-                                            <Td>
-                                                <Button
-                                                    as="a"
-                                                    size="sm"
-                                                    fontSize="sm"
-                                                    colorScheme="purple"
-                                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                                >
-                                                    Editar
-                                                </Button>
-                                            </Td>
-                                        </Tr>
-                                        <Tr>
-                                            <Td px={["4", "4", "6"]}>
-                                                <Checkbox colorScheme="pink"></Checkbox>
-                                            </Td>
-                                            <Td>
-                                                <Box>
-                                                    <Text fontWeight="bold">Rafael araujo</Text>
-                                                    <Text fontSize="sm" color="gray.300">rafa12@gmail.com</Text>
-                                                </Box>
-                                            </Td>
-                                            {isWideVersion && <Td>04 de abril, 2023</Td>}
-                                            <Td>
-
-                                                <Button
-                                                    as="a"
-                                                    size="sm"
-                                                    fontSize="sm"
-                                                    colorScheme="purple"
-                                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                                >
-                                                    Editar
-                                                </Button>
-
-                                            </Td>
-                                        </Tr>
+                                        {
+                                            data.map(user => {
+                                                return (
+                                                    <Tr key={user.id}>
+                                                        <Td px={["4", "4", "6"]}>
+                                                            <Checkbox colorScheme="pink"></Checkbox>
+                                                        </Td>
+                                                        <Td>
+                                                            <Box>
+                                                                <Text fontWeight="bold">{user.name}</Text>
+                                                                <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                            </Box>
+                                                        </Td>
+                                                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                                                        <Td>
+                                                            <Button
+                                                                as="a"
+                                                                size="sm"
+                                                                fontSize="sm"
+                                                                colorScheme="purple"
+                                                                leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                                            >
+                                                                Editar
+                                                            </Button>
+                                                        </Td>
+                                                    </Tr>
+                                                )
+                                            })
+                                        }
                                     </Tbody>
                                 </Table>
 
