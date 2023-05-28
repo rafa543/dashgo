eaea4053b51d1846c630f948a0c034d5a22a0707
Rafael Araujo <rafa123839@gmail.com>
2023-05-28 08:04:04 -0300

    Configurando Axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,11 +5,11 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
 import { useQuery } from 'react-query'
+import { api } from "../../services/api";
 
 export default function UserList() {
     const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json()
+        const { data } = await api.get('users')
 
         const users = data.users.map(user => {
             return {
@@ -44,7 +44,7 @@ export default function UserList() {
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">
                             Usuários
-                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
                         </Heading>
 
                         <Link href="/users/create" passHref>
